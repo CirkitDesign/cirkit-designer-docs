f7a5cffca04fb0cbac03db3a825ae46823a9f997
unknown <austinsmall326@comcast.net>
2025-03-17 22:17:49 -0400

    switch from tutorials back to docs

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/tutorials/',
+  baseUrl: '/docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -36,7 +36,7 @@ const config: Config = {
       'classic',
       {
         docs: {
-          path: 'tutorials',
+          path: 'docs',
           routeBasePath: '/',
           sidebarPath: './sidebars.ts',
         },
@@ -78,7 +78,7 @@ const config: Config = {
           items: [
             {
               label: 'Tutorial',
-              to: '/tutorials/intro',
+              to: '/docs/intro',
             },
           ],
         },
